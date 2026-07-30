e4fdd7bd87e2ae26669b36b30e0c27af1b3917aa
Trevor Gituru <giturutrevor@gmail.com>
2026-07-30 11:12:04 +0300

    Configure module manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,34 +1,27 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "meter_invoice",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
+    "name": "Meter Invoice",
+    "summary": "Adds meter reading support to customer invoices",
+    "description": """
+Extends the Odoo Accounting module by adding Previous, New,
+and Actual meter reading fields to invoice lines. The module
+automatically calculates consumption, updates the invoice
+quantity, and displays the readings on invoice views and reports.
     """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+    "author": "Trevor Gituru",
+    "website": "https://github.com/trevor-gituru/odoo-meter-reading-module",
+    "category": "Accounting",
+    "version": "18.0.1.0.0",
+    # Required module
+    "depends": ["account"],
+    # Data files loaded during installation
+    "data": [
+        "views/views.xml",
+        "views/templates.xml",
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    # Demo data
+    "demo": [],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
 }
-
